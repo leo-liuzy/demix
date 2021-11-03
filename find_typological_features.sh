ef62a9645951d97e66fc28d08eedfccaff5dd7a2
Leo Liu <zeyuliu@devfair0253.h2.fair>
2021-11-03 13:53:39 -0700

    update local changes

--- find_typological_features.sh
+++ find_typological_features.sh
@@ -12,13 +12,14 @@ export NUM_EVALUATION_GPUS=8;
 export NEW_LANG_DIR=$SERIALIZATION_DIR/unseen_langs
 export SEEN_LANGS=en_XX,fr_XX,zh_CN,ru_RU,ja_XX,id_ID,ro_RO,de_DE
 # alphabetically sorted: ['de_DE', 'en_XX', 'fr_XX', 'id_ID', 'ja_XX', 'ro_RO', 'ru_RU', 'zh_CN']
-for NEW_DOMAIN in nl_XX fy_NL ko_KR my_MM es_XX gl_ES pl_PL be_BY
-do  
-    echo "Language: ${NEW_DOMAIN}"
-    export DEV_POSTERIOR_OUTPUT="${NEW_LANG_DIR}/${NEW_DOMAIN}/dev_posteriors.jsonl"
-    # bash demix/mix_eval_lm.sh $NUM_EVALUATION_GPUS $NEW_DATA_BIN  ${SERIALIZATION_DIR}/checkpoint_last-rank-0.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-1.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-2.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-3.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-4.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-5.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-6.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-7.pt $NEW_DOMAIN $DEV_POSTERIOR_OUTPUT estimate > ${NEW_LANG_DIR}/${NEW_DOMAIN}/estimate_eval_len1024.log 2>&1
-    export POSTERIOR=$(tail -n 1 $DEV_POSTERIOR_OUTPUT | jq -rc '.exp_avg_posterior | join(",")')
-    python scripts/find_best_matching_typological_feature.py --seen-languages $SEEN_LANGS --demix-posterior $POSTERIOR --target-language $NEW_DOMAIN --result-dir ${NEW_LANG_DIR}/${NEW_DOMAIN}
-    echo
-done
+# for NEW_DOMAIN in nl_XX fy_NL ko_KR my_MM es_XX gl_ES pl_PL be_BY
+# do  
+#     echo "Language: ${NEW_DOMAIN}"
+#     export DEV_POSTERIOR_OUTPUT="${NEW_LANG_DIR}/${NEW_DOMAIN}/dev_posteriors.jsonl"
+#     # bash demix/mix_eval_lm.sh $NUM_EVALUATION_GPUS $NEW_DATA_BIN  ${SERIALIZATION_DIR}/checkpoint_last-rank-0.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-1.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-2.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-3.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-4.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-5.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-6.pt:${SERIALIZATION_DIR}/checkpoint_last-rank-7.pt $NEW_DOMAIN $DEV_POSTERIOR_OUTPUT estimate > ${NEW_LANG_DIR}/${NEW_DOMAIN}/estimate_eval_len1024.log 2>&1
+#     export POSTERIOR=$(tail -n 1 $DEV_POSTERIOR_OUTPUT | jq -rc '.exp_avg_posterior | join(",")')
+#     python scripts/find_best_matching_typological_feature.py --seen-languages $SEEN_LANGS --demix-posterior $POSTERIOR --target-language $NEW_DOMAIN --result-dir ${NEW_LANG_DIR}/${NEW_DOMAIN}
+#     echo
+# done
 # set NUM_EVALUATION_GPUS equal to the number of experts you'd like to ensemble.
+python scripts/find_best_matching_typological_feature.py --seen-languages $SEEN_LANGS --target-languages nl_XX,fy_NL,ko_KR,my_MM,es_XX,gl_ES,pl_PL,be_BY --unseen-langs-dir ${NEW_LANG_DIR}
--- scripts/find_best_matching_typological_feature.py
+++ scripts/find_best_matching_typological_feature.py
@@ -14,6 +14,7 @@ import json
 from scipy.special import softmax
 from scipy.spatial import distance as spatial_distance
 from scipy.stats import wasserstein_distance
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd 
 
@@ -48,66 +49,102 @@ def main(eps=EPS):
     parser.add_argument('--seen-languages', required=True,
                         default='en_XX,fr_XX,zh_CN,ru_RU,ja_XX,id_ID,ro_RO,de_DE', type=str,
                         help='Comma seperated list of seen languages (e.g. language used on training).')
-    parser.add_argument('--target-language', required=True,
-                        help='Target language of interest')
-    parser.add_argument('--demix-posterior', required=True,
-                        help='posterior distribution from DEMix; Expected input is a comma seperated and valid probability distribution')
+    parser.add_argument('--target-languages', required=True,
+                        help='Comma seperated list of interested target language')
+    # parser.add_argument('--demix-posterior', required=True,
+    #                     help='posterior distribution from DEMix; Expected input is a comma seperated and valid probability distribution')
     # parser.add_argument('--divergence-metric', required=True,
     #                     help='metric used for measuring divergence between two probability distribution')
-    parser.add_argument('--result-dir', help="This script will save intermediate search results in a table and"\
+    parser.add_argument('--unseen-langs-dir', help="This script will save intermediate search results in a table and"\
                         "save this table to the provided directory")
     # fmt: on
     args = parser.parse_args()
     print(args)
-    assert os.path.exists(args.result_dir)
+    assert os.path.exists(args.unseen_langs_dir)
     assert len(args.seen_languages) > 0 and ',' in args.seen_languages
     seen_langs = sorted(args.seen_languages.split(','))
+    target_langs = sorted(args.target_languages.split(',')) # 
+    demix_posteriors = []
+    for target_lang in target_langs:
+        dev_posterior_filepath = f"{args.unseen_langs_dir}/{target_lang}/dev_posteriors.jsonl"
+        dev_posteriors = open(dev_posterior_filepath, "r").readlines()
+        dev_posterior = json.loads(dev_posteriors[-1])['exp_avg_posterior']
+        demix_posteriors.append(dev_posterior)
+        
     uniform_dist = np.ones(len(seen_langs)) / len(seen_langs)
-    demix_posterior = string2list(args.demix_posterior)
+    # demix_posterior = string2list(args.demix_posterior)
     results = []
     header = ["seen_langs", 'target_lang', 'demix_posterior', 'typological_distance_metric', 'typological_distance', 
               'typological_similarity', 'approximated_posterior', 'divergence_metric', 'divergence', 'divergence_with_uniform']
     assert all('_' in lang for lang in seen_langs)
-    assert '_' in args.target_language
+    assert all('_' in lang for lang in target_langs)
+    # assert '_' in args.target_language
     seen_langs = list(map(lambda x: LETTER_CODES[x.split('_')[0]], seen_langs))
-    # print(seen_langs)
-    target_lang = LETTER_CODES[args.target_language.split('_')[0]]
+    # target_langs = list(map(lambda x: LETTER_CODES[x.split('_')[0]], target_langs))
         
     for divergence_metric in DIVERGENCE_METRICS:
         print(f"Using {divergence_metric} divergence metric:")
         divergence_func = divergence_func_from_name(divergence_metric)
-        divergence_with_uniform = divergence_func(demix_posterior, uniform_dist)
+        distance_metric2langs = {m: [] for m in TYPOLOGICAL_DISTANCE_METRICS}
+        lang2hitmiss = {}
+        for i, target_lang_fb in enumerate(target_langs):
+            print(f"Target language: {target_lang_fb}")
+            assert '_' in target_lang_fb  # fb lang code has a '_' in it
+            target_lang = LETTER_CODES[target_lang_fb.split('_')[0]]
+            demix_posterior = demix_posteriors[i]
 
-        min_divergence = float('inf')
-        min_typological_distance_metric = None
-        min_approximated_posterior = None
-        distance_metric2langs = {}
-        
-        for distance_metric in TYPOLOGICAL_DISTANCE_METRICS:
-            distance_matrix = lang2vec.distance(distance_metric, [target_lang] + seen_langs)
-            # first 0 is selecting the first row; 1 is truncating the target_lang
-            if distance_metric
-            distance = distance_matrix[0][1:]
-            # print(f"{distance}: distance")
-            similarity = 1 - distance + eps # turn distance measure to similarity measure since distance is in [0, 1], eps added for case of all-zero
-            approximated_posterior = similarity / similarity.sum() # softmax(similarity)
-            divergence = divergence_func(demix_posterior, approximated_posterior)
-            if divergence < min_divergence:
-                min_divergence = divergence
-                min_typological_distance_metric = distance_metric
-                min_approximated_posterior = approximated_posterior
-            results.append([args.seen_languages, args.target_language, demix_posterior, distance_metric, distance,
-                            similarity, approximated_posterior, divergence_metric, divergence, divergence_with_uniform])
+            divergence_with_uniform = divergence_func(demix_posterior, uniform_dist)
+            min_divergence = float('inf')
+            min_typological_distance_metric = None
+            min_approximated_posterior = None
+            for distance_metric in TYPOLOGICAL_DISTANCE_METRICS:
+                distance_matrix = lang2vec.distance(distance_metric, [target_lang] + seen_langs)
+                # first 0 is selecting the first row; 1 is truncating the target_lang
+                distance = distance_matrix[0][1:]
+                # print(f"{distance}: distance")
+                similarity = 1 - distance + eps # turn distance measure to similarity measure since distance is in [0, 1], eps added for case of all-zero
+                approximated_posterior = similarity / similarity.sum() # softmax(similarity)
+                divergence = divergence_func(demix_posterior, approximated_posterior)
+                if divergence < min_divergence:
+                    min_divergence = divergence
+                    min_typological_distance_metric = distance_metric
+                    min_approximated_posterior = approximated_posterior
+                results.append([args.seen_languages, target_lang_fb, demix_posterior, distance_metric, distance,
+                                similarity, approximated_posterior, divergence_metric, divergence, divergence_with_uniform])
+            # here, count how many times one metric is best for one language
+            distance_metric2langs[distance_metric].append(target_lang_fb)
+            lang2hitmiss[target_lang_fb] = [distance_metric == min_typological_distance_metric for distance_metric in TYPOLOGICAL_DISTANCE_METRICS]
 
-        print(f"min_typological_distance_metric: {min_typological_distance_metric}")
-        print(f"min_divergence: {min_divergence}")
-        print(f"divergence_with_uniform: {divergence_with_uniform}")
-        print(f"min_approximated_posterior: {min_approximated_posterior}")
-        print(f"demix_posterior: {demix_posterior}")
-        print()
-    
+            print(f"min_typological_distance_metric: {min_typological_distance_metric}")
+            print(f"min_divergence: {min_divergence}")
+            print(f"divergence_with_uniform: {divergence_with_uniform}")
+            print(f"min_approximated_posterior: {min_approximated_posterior}")
+            print(f"demix_posterior: {demix_posterior}")
+            print()
+        # Different divergence metric leads to different best distance metric, so plot it for different divergence metric
+        plt.title(f"Best distance metric by {divergence_metric} divergence")
+        plt.bar(distance_metric2langs.keys(), [len(v) for v in distance_metric2langs.values()])
+        plt.xlabel("Name of distance metric (in lang2vec)")
+        plt.ylabel("No. of times seelcted as the best matching metric")
+        plt.savefig(f"{args.unseen_langs_dir}/best_distance_metric_stats_by_{divergence_metric}.pdf", format="pdf")
+
+        # also have a scatter plot
+        lang2hitmiss = pd.DataFrame(lang2hitmiss)
+        lang2hitmiss = lang2hitmiss.transpose()
+        lang2hitmiss.columns = TYPOLOGICAL_DISTANCE_METRICS
+        plt.title(f"Best distance metric by {divergence_metric} divergence")
+        heatmap = plt.imshow(lang2hitmiss)
+        plt.xticks(range(len(lang2hitmiss.columns.values)), lang2hitmiss.columns.values)
+        plt.yticks(range(len(lang2hitmiss.index)), lang2hitmiss.index)
+        cbar = plt.colorbar(mappable=heatmap, ticks=[0, 1], orientation='vertical')  
+        # vertically oriented colorbar
+        cbar.ax.set_yticklabels(['Not best', 'Best']) 
+        plt.xlabel("Name of distance metric (in lang2vec)")
+        plt.ylabel("Name of unseen language")
+        plt.savefig(f"{args.unseen_langs_dir}/best_distance_metric_heatmap_by_{divergence_metric}.pdf", format="pdf")
+
     df = pd.DataFrame(results, columns=header)
-    df.to_csv(f"{args.result_dir}/typological_search_results.tsv", sep="\t")
+    df.to_csv(f"{args.unseen_langs_dir}/typological_search_results.tsv", sep="\t")
     
 
 
